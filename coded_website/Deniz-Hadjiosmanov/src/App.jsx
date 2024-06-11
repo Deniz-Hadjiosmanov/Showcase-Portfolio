e8d16468a37b858f6414fb7c3b545a73aa2e274f
Deniz Hadjiosmanov <d.hadjiosmanov@student.fontys.nl>
2024-06-11 07:14:17 +0200

    Made a component for the Contact section

--- coded_website/Deniz-Hadjiosmanov/src/App.jsx
+++ coded_website/Deniz-Hadjiosmanov/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Home from './components/Homepage'
 import Projects from './components/Projects'
 import About from './components/About'
+import Contact from './components/Contact'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Home/>
       <Projects/>
       <About/>
+      <Contact/>
     </>
   )
 }
---
+++ coded_website/Deniz-Hadjiosmanov/src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Contact.css';
+
+function Contactsection() {
+    return (
+    <>
+    <section id="contact-section-container">
+        
+        
+    </section>
+    </>
+    )
+}
+
+export default Contactsection;
